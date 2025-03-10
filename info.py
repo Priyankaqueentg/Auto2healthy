589103ab1da0a6abd848574b536d94e4a8b6edae
Oo <officialwildcash4@gmail.com>
2025-03-10 11:30:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '25847347'))
-API_HASH = environ.get('API_HASH', '11569826ebbc9f8b14c3ab1c5ddb937b')
+API_ID = int(environ.get('API_ID', '1740849725'))
+API_HASH = environ.get('API_HASH', '680786593eb3e6b3195153f1ae9dd17e')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
